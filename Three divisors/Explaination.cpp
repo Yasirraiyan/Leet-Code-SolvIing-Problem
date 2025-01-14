fa16810f4b49c6a8598c046fac4043d6fe5d8cca
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-14 19:41:55 +0600

    Create Explaination.cpp

---
+++ Three divisors/Explaination.cpp
@@ -0,0 +1,5 @@
+if n=4;
+if we check up to less than  equal 4 from 1
+  now 1<4 it is true;
+      and 2<4 ;4==4; all true and for 1,2,4 if we mod 4 by 1,2,4 ans is true for every condition count+++ at last they are 3
+but 3<4 but 4%3==0 is false not add with count
