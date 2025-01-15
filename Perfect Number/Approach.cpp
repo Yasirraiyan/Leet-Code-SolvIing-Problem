40d5910951c516666d9a82ef50b0681ae49e1c92
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-15 13:09:18 +0600

    Create Approach.cpp

---
+++ Perfect Number/Approach.cpp
@@ -0,0 +1,11 @@
+1 ta number input nibo
+etar sobgula multiple ber korbo exa:
+28=1X28=2X14=4x7
+  ei jonno for loop iterate korbo 1 to n
+check condition n%i==0;
+if true print it;
+sum variable nei initial==0;
+n%i==0 true hole sum+=i kori every time;
+if sum==n;
+return true;
+else return false;
