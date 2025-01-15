672a4a588026ef3962795dff5cd9fca2ccbb0803
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-15 18:57:17 +0600

    Create Approach.cpp

---
+++ Calculate Delayed Arrival Time in Leetcode/Approach.cpp
@@ -0,0 +1,12 @@
+Amar 2ta veriable ase arrivaltime,delayedtime
+  Delayed arrival time hoilo ei duitar addition.
+  kintu jodi delayedarrivaltime 12 er beshi hoy taile jemon 24 hoile 0 return kore 13 hoile 1 return kore
+  so
+13%24==1;
+24%24==0;
+so tokhon amra ekta condtion dibo je 
+  if(delayedarrivaltime>12)
+  {
+return delayedarrivaltime%24;
+}
+  
