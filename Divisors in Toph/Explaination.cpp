3e7b703cd305a355cad6fc6ce651da47e70302fe
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-15 11:34:17 +0600

    Create Explaination.cpp

---
+++ Divisors in Toph/Explaination.cpp
@@ -0,0 +1,7 @@
+1 integer which is n
+Print all integer by which n is divisable;
+So i iterate a for loop from 1 to n
+check condition (n%i)==0 if true print i;
+because for example 
+6=1X6=2X3;
+so for 6 it is 1,2,3,6
