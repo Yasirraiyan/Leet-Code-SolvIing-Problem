30225774dd10254c04ba324bbe6b252039a8bf7a
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-15 23:10:14 +0600

    Create Explaination.cpp

---
+++ Factorial Trailing Zero/Explaination.cpp
@@ -0,0 +1,21 @@
+Factorial trailing zero etar mane 
+ekta number input nibo eitar factorial bair korbo
+Tai i=1 Theke n porjoto for loop chalai
+then fact*i kori. 
+  check r jonno bool is trailling nibo variable ja initially false hobe
+
+  initial fact=1;
+erpor check korbo fact%5=0 kina
+karon seshe 0 thakle 5 dia vag jay
+othoba fact℅10=0 check kora jay
+if 
+true hoy 
+count variable rakhbo trailing zero koyta oitar 
+hishaber jonno 
+initial cont=0;
+bool istrailing=true kori
+protibar count+=1
+  kori
+ckunt print kori
+else 
+false.
