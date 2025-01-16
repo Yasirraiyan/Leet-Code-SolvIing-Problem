3c2ff24cc494728438711c4018143da3aad85755
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-16 21:22:52 +0600

    Create Explaination.cpp

---
+++ Leetcode 1342 Number of steps to Reduce a Number to Zero/Explaination.cpp
@@ -0,0 +1,16 @@
+Amar 1 ta number zero na hwa porjonto iteration chalabo
+So ami ekta while loop net jekhane condition dibo num>0
+Erpor Given than if number is even divide it by 2
+So i give a condition that (num%2==0)
+  if it is true perform num=num/2;
+Ar jodi number ta odd hoy tahole given reduce it by 1
+  So i give another conditiion if statement than if(num%2==1) or if(num%2!=0)
+     perform num=num-1;
+or direct perform else and same condition instead of step 7;
+Number even odd jai hok protibar step gonona korbo tai ami count 1 ta variable rakhi ja initially 0 as before start operation no step counted .If step performed then count it by  increasing 1.So,perform 
+  count++
+  and then
+  while loop er moddhe 
+count++ dei;
+Amar chaise kotota step lage reduce korte
+So,at last i return count
