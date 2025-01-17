79fcb7846335375d33025fc71b8869c49284e29f
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-17 11:34:49 +0600

    Create Approach.cpp

---
+++ Leetcode 136 Single Number/Approach.cpp
@@ -0,0 +1,11 @@
+1 ta array ase.Kono element 1 bar er beshi ase konota 1 bar ase
+amr lagbe kon element ta 1 bar ase ta return korbo.
+  1st e ekta for loop chalai pura array traverse kor i=0 to i=n porjonto
+aro 1 ta for loop chalai ja nested j=0 to j=n porjonto karon 1 bar purAa array traverse kori then abar array r 0 index theke sobar sathe compare kori
+erpor check kori je arr[i]=arr[j] kina jodi same hoy tar mani oi element 1 er beshi bar ase
+1 ta count variable nei ja initial 0 eta lagbe karon 1 ta hishab rakhbo j eita 1 er beshi kina protibar count++ kori
+  amr chaise oi index er value so 
+check kori je jodi count==1 hoy mane 
+if(count==1) hoy 
+  return kori oi index er element mane
+arr[i];
