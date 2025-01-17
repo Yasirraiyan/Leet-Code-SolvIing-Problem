91b97f91b408000cb42e4fdff3cfdb92e1134ef6
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-17 10:43:40 +0600

    Create Approach.cpp

---
+++ LeetCode 412 FizzBuzz/Approach.cpp
@@ -0,0 +1,16 @@
+1 ta array ase 
+  if i is divisible by 3 and 5
+  hoy taile return '"FizzBuzz";
+  if i is divisible by 3
+return "Fizz";
+if i is divisible by 5
+  return "Buzz";
+Taile ami 1ta for  loop chailai index i=0 theke n porjonto 
+protibar i++;
+check kori je 
+if(i%3==0&&i%5==0)
+  return kori "FizzBuzz";
+if(i%3==0)
+  return kori "Fizz";
+if(i%5==0)
+  return kori "Buzz";
