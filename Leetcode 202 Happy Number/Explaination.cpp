57930d01023ac22ea2ccdc14d605ce17786d5c46
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-17 09:49:39 +0600

    Create Explaination.cpp

---
+++ Leetcode 202 Happy Number/Explaination.cpp
@@ -0,0 +1,16 @@
+Happy number emon number ja positive mane num>0
+Ekhn oi nambar er digit gular borgo kore add korte thakbo again and agin untill it not equal 1.
+
+So, i iterate a while loop where condition is n!=1;
+ekhn n=n%10
+then protibar n=n/10;
+then sum variable rakhbo ja initially 0
+then sum+=n*n;
+kori
+karon for 19=1^2+9^2
+then it is 82 for 82 it is=8^2+2^2;
+then it is 68 then for 68 it is 6^2+8^2=100
+then 1^2+0^2+0^2=1
+complete return true
+  else
+return false
