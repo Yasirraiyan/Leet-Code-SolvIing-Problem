fcc6466b2a0fab4b7da31e7cfffa2401c451acf6
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-17 08:52:53 +0600

    Create Approach.cpp

---
+++ Leetcode 628 Maximum Product of thtree numbers/Approach.cpp
@@ -0,0 +1,7 @@
+nums=[1,2,3] output:6=1x2x3=nums[0]*nums[1]*nums[2]
+nums=[1,2,3,4] output:24=1x2x3x4=nums[0]*nums[1]*nums[2]*nums[3]
+so ekhane array r proti index er value multiply hocche
+initiak product=1; karon jodi kisu mutiply nao hoy 1 return kore
+so i iterate a for loop from i=0 up to n
+then product*arr[i] every time
+return product;
