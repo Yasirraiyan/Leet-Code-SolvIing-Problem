a6a564578a796128e68ab4ffa1e8b03fd54c2203
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-17 08:32:35 +0600

    Create Approach.cpp

---
+++ Leetcode 70 Climbing stairs/Approach.cpp
@@ -0,0 +1,10 @@
+amar ekhane koto step e jaite parbo oitar count lagbe
+Exa:
+n=3 hoy 
+  tale
+3=1+1+1=2+1=1+2
+  amar tar mane n er multipls gula ber kora lagbe;
+so i=1 theken n porjonto for loop chalai protibar if(n%i==0) thei
+  karon n%i==0 dile true hoile oi i ta hbe oi n er multiple;
+protibar count++ kori
+  seshe return kori count;
