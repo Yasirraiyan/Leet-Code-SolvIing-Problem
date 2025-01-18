b185ddf70ceec2194cc274430cf5c2c75390dd77
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-19 00:25:40 +0600

    Create Approach.cpp

---
+++ Leetcode 485 Max Consecuative 1s/Approach.cpp
@@ -0,0 +1,11 @@
+Amar 1 ta array ase.Amr lagbe kotota 1 porpor ase count kora
+1. 1ta variable nibo count ja initial 0.  1 count er jonno
+2.Erpor 1 ta for loop chalai i=0 to i<n porjonto array travarse er jonno
+3.nested for loop chalai j=0 to j<n porjonto.
+4.Check kori condition je
+if(i=j+1) hoy karon 0th index 1th index mani porpor dui index er value same hote pare
+then check kori 
+if(nums[i]==1 && nums[j]===1)
+jodi true hoy count++ kori
+return kori count
+karon amar koyta ase ta chaise
