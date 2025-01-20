aab5e9c9f9d40e8848a81fec8b499555486cf53e
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-20 22:51:31 +0600

    Create Approach.cpp

---
+++ Leetcode 1550 Three Consecutive odds/Approach.cpp
@@ -0,0 +1,18 @@
+Amar 1ta array ase.Amr check korte hbe por por 3 ta index e odd number ase kina.
+  So,
+  ami 1ta for loop chalai i=0 to i=n porjonto.
+  por por 3ta index dhori i,i+1,i+2
+  ba i-2,i-1,i;
+then check kori 3 index er value odd kina 
+so, condition dibo 3 index er value jodi 2 dia divide na jay reminder 0 na pai tale true.mane iscontain=true;
+  1 ta variable raki contain ja bool type mane bool contain.
+  So,condition
+   if(arr[i] % 2 != 0) && (arr[i + 1] % 2 != 0) && (arr[i + 2] % 2 != 0)) {
+                iscontain = true;
+}
+na hoile false.
+  So,
+  else
+  iscontain=false;
+Last e return kori iscontain.
+  
