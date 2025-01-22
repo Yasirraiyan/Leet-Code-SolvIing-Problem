b22e9473dd17f00ab2adbd65c6cb154f36621d31
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-22 19:42:03 +0600

    Create Approach.cpp

---
+++ Leetcode 2475 Number of unequal Tripltes on an array/Approach.cpp
@@ -0,0 +1,12 @@
+Condition 0<i<j<k
+so 1 ta for loop chalai i=0 to n porjonto.
+Nested for loop chalai as i<j
+j=i+1 to n porjont
+as j<k
+so k=j+1 to n porjonto
+if chalai condition
+if (0 <= i && i < j && j < k && nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) 
+trude hoile
+count++;
+chaise count initial count=0;
+return count.
