11f7516be38f65b1cb9379424b18980c6405b1b2
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-22 19:11:16 +0600

    Create Approach.cpp

---
+++ Leetcode 2455 Avarage Value of Even Numbers That are Divisible by Three/Approach.cpp
@@ -0,0 +1,24 @@
+Amar 1 ta array ase. Amar chaise ei array r even number gular avarage value jeta abar 3 dia divide korle reminder 0 ase.
+So,
+  first e 1 ta for loop chalai i=0 to n porjonto array traverse er jonno.
+  ekhn check korbo array r kon value even.
+  so,
+  perform arr[i]%2
+  operation is
+if(arr[i]%2==0)
+jodi true hoy eta even er jonno
+kintu aro 1 ta condition ase eita abar 3 dia divide jaite hbe
+so,check both 2 and 3.
+  So,perform code
+if(arr[i]%2==0 && arr[i]%3==0)
+jodi true hoy count++;
+count keno nei
+chaise avarage so sum k number gula kotogula total oita dia divide korbo
+so,count++;
+last e perform operation (sum/count)
+eitake hishaber subidharte result name variable rakhi
+So,
+result=(sum)/(count);'
+  last e result return kori.
+  So,
+  return result;
