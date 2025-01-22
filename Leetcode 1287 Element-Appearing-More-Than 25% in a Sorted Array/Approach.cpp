51fb97b9602da43ca61683b98281643cea010b29
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-22 12:37:22 +0600

    Create Approach.cpp

---
+++ Leetcode 1287 Element-Appearing-More-Than 25% in a Sorted Array/Approach.cpp
@@ -0,0 +1,13 @@
+First e count variable nei ja initial 0.Eta die count korbo koybar ase kon element.
+1 ta for loop chalai i=0 to n porjonto.Then nested for loop chalai j=0 to n porjonto.
+Condition check kori 1 bar er beshi appear kina
+So,
+  if(arr[i]==arr[j]) kina jodi true hoy count++ kori.
+  arr.size() dia array r size ber kori
+  arekta variable rakkhi count2 ja die percantage count kori
+and
+count2=count/arr.size();
+check kori condition je count2 25% er beshi kina ba 0.25 er beshi kina
+if(count2>0.25) 
+true hoile 
+return kori arr[i].
