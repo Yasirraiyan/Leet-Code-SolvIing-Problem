0a3b9d79d69777d003039c534d7182ce54463765
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-23 15:53:42 +0600

    Create Approach.cpp

---
+++ Leetcode 219 Contains Duplicate-II/Approach.cpp
@@ -0,0 +1,4 @@
+Amar 1 ta array ase nums.Compare kori nums[i]==nums[j]
+1 ta loop chalai for i=0 to n porjonto
+j=i+1 to n porjonto nested for chalai//  j=i+1 karon j=0 hole nijer sathe nije compare kore
+check kori if(abs(i-j)<=k) joi hoiy nested if (nums[i]==nums[j]) hoy taile true return naile false.
