49aa2056c2ee24fcdd6eafa4ea09ebff5793b7a4
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-23 11:28:52 +0600

    Create solution.cpp

---
+++ Leetcode 1534 Count5 Good TRiplets/solution.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+#include<cmath>
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) 
+    {
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            for(int j=i+1;j<arr.size();j++)
+            {
+                for(int k=j+1;k<arr.size();k++)
+                {
+                   if ((0 <= i) && (i < j) && (j < k) && (k < arr.size()))
+                    {
+                        if((abs(arr[i] - arr[j])<=a)&&(abs(arr[j] - arr[k])<=b)&&(abs(arr[i] - arr[k])<=c))
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+            }
+        }
+        return count;
+    }
+};
