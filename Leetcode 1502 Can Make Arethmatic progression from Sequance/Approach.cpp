f01114c945ca57386dff7750cc558e18add20871
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-23 16:15:19 +0600

    Create Approach.cpp

---
+++ Leetcode 1502 Can Make Arethmatic progression from Sequance/Approach.cpp
@@ -0,0 +1,10 @@
+Amar arethmatic sequence e sobar differance equal.
+  Array ase 1 ta 
+for loop chalai array travarse kori
+agey differance ber kori
+diff=arr[1]-arr[0];
+check kori loop er moddhe 
+arr[i+1]-arr[i]==diff kina 
+if true retirn true.
+  else
+  return false;
