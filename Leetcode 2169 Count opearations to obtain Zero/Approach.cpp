37c862de580a710ae07878f4ef6613e1d61e259d
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-27 23:06:51 +0600

    Create Approach.cpp

---
+++ Leetcode 2169 Count opearations to obtain Zero/Approach.cpp
@@ -0,0 +1,6 @@
+Amar 2 ta number ase. ja non negative. Jodi num1>num2 hoy num1 theke num2 bad dibo.Naile num2 theke num1 bad dibo.
+  kototi operation lage ta count korbo.
+  SO, ami 1 ta count variable rakhi ja initial 0.
+Sobar jonno count++ kori
+  return kori count.
+  
