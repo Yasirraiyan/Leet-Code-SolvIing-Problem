553cb664d958d8a9ba2d44a7c4b6a8dfafd4cd19
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-27 19:41:52 +0600

    Create Approach.py

---
+++ Leetcode 349 Intersection of two array/Approach.py
@@ -0,0 +1,12 @@
+Amar 2 ta array ase.Intersection common element ber korbo.
+So,python dia 
+array k list e nei
+Nums1=list(nums1)
+Nums2=list(nums2)
+List k set e nei
+n1=set(Nums1)
+n2=set(Nums2)
+intersection ber kori
+ commmon=n1&n2
+array chaise so common k list e convert kori return kori
+return list(commmon)
