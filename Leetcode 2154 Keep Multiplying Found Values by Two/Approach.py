65f6a21fa509ee09e992cc9e5a37eea16a0b4f2e
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-27 22:43:04 +0600

    Create Approach.py

---
+++ Leetcode 2154 Keep Multiplying Found Values by Two/Approach.py
@@ -0,0 +1,11 @@
+Amar 1 ta array ase jar nam nums.
+1 ta int ase iriginal ja k multiply korbo 2 dia jotokkhon na pai multiply result array te.
+Bar bar kori so,
+1 ta while loop chalai
+while kotokkhonn cholbe jotokkhon na paI
+
+So,
+while not in nums:
+  original=original*2;
+  
+  
