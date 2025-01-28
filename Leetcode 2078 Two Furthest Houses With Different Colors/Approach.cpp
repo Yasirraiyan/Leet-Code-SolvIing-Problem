8c3f9258f2919fd0a4c4f973d18881b39129147d
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-28 12:05:24 +0600

    Create Approach.cpp

---
+++ Leetcode 2078 Two Furthest Houses With Different Colors/Approach.cpp
@@ -0,0 +1,12 @@
+Amar 1 ta array ase colors.Ei array r index value jokhon change hbe tar mani color change hoise.Kintu amar lagbe maximum.
+So,first ami 1 ta for loop chalai i=0 to i<n porjonto pura array traverse er jonno.
+  Erpor nested for loop chalai j=i+1 to n porjonto
+j=i+1 karon ami 1 ta index er sathe oi same index compare korbo na 
+dekhbo ith index value ar jth index value ki different kina jodi different hoy
+taile abs(i-j) ber kori eta ke distance vriable e rakhi
+kintu chaise max_distance;
+initial max_distance =0 rakhi
+ber kori max(distance,max_distance) eta ke ans variable  e rakhi
+return kori ans;
+max ber korar jonno library import kori
+#include<algorithm>
