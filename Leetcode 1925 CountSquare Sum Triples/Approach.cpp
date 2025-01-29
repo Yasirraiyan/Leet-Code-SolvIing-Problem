e637eb4d8696854abafadb7b154182a891258d1b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-29 12:27:50 +0600

    Create Approach.cpp

---
+++ Leetcode 1925 CountSquare Sum Triples/Approach.cpp
@@ -0,0 +1,15 @@
+Amar 1 ta number ase eta k ki pythagorus mane kina check 
+mane a^2+b^2=c^2 kina  check.
+  ekhane 1<=a,1<=b,1<=c <=n 
+  so,
+  1 ta for loop chalai a=1 to a<=n porjonto and a++;
+  2nd for loop chalai nested b=1 to b<=n porjonto b++;
+3rd for loop chalai nested c=1 to c<=n porjonto c++;
+check kori 
+if(a*a+b*b==c*c)
+                {
+                    count++;
+                }
+count return kori.
+amar chaise koyta ase .
+  first w inituial count=0; rakhi.
