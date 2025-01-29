ff9d9717919ab18801f860e691af497a201229ac
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-29 15:31:19 +0600

    Update Acceptance.cpp

--- LeetCode 412 FizzBuzz/Acceptance.cpp
+++ LeetCode 412 FizzBuzz/Acceptance.cpp
@@ -1,3 +1,4 @@
 Solve my myself successfully without seeing any solution or help anyother.
-  Accepted Successfully.
+  Accepted Successfully.Solution1 and Solution2 both are accepted.
 Solution 2 also created by me successfully without seeing anything.
+Solution 2 also Accepted.
