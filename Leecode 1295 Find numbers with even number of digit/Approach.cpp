afa82fd013a987e7d7bbb57ac5117385b6094ba5
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-29 12:02:29 +0600

    Create Approach.cpp

---
+++ Leecode 1295 Find numbers with even number of digit/Approach.cpp
@@ -0,0 +1,14 @@
+Amar 1 ta array ase .First e array te 1 ta for loop chalai pura array traverse kori.for er baire 1 ta variable nei count1 ja initial 0.Eta dia count korbo kototi element array te even number of digit.
+  for er vitore arekta variable nibo count=0; eta die count korbo digit number array r index er valuer.for er vitore 0 karon jeno 1st traverse e kisu count na kore.
+for er vitore 1 ta while loop chailai 
+while(nums[i]!=0)
+        {
+            nums[i]=nums[i]/10;
+            count++;
+        }
+as bolse even number digit tai check kori 
+if(count%2==0)
+        {
+            count1++;
+        } 
+return kori count1.
