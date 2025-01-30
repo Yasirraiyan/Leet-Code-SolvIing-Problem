9112751d81d87eaab34c46a56a176478335fa416
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-30 10:06:45 +0600

    Create Approach.cpp

---
+++ Leetcode 3162 Find the Number of Good Pairs I/Approach.cpp
@@ -0,0 +1,7 @@
+Amar 2 ta array ase nums1 and nums2 . nums1 er length n,nums2 er length m.Ar 1 ta integer dea ase k.
+  and bolse 
+(0 <= i <= n - 1, 0 <= j <= m - 1).
+  pair good hbe jodi 
+A pair (i, j) is called good if nums1[i] is divisible by nums2[j] * k true hoy ;[For this i perform operation (nums1[i]%(nums2[j]*k)==0)
+  jdi true hoy taile count++ kori.Karon amar chaise kototi.So, ami 1 ta variable nei count ja initial 0.
+return kori count.
