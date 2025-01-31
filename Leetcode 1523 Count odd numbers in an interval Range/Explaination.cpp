7f195c2f83ba7cf9854aa527fa5a5f0d650be361
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-31 23:00:18 +0600

    Create Explaination.cpp

---
+++ Leetcode 1523 Count odd numbers in an interval Range/Explaination.cpp
@@ -0,0 +1,9 @@
+Amar 2 ta integer ase low ar high.
+  amar high ar low er majhe kotogula odd number ae ta count korbo
+firste initial 1 ta variable nai count=0;
+for loop iterate kori i=low to i<=high porjonto
+protibar i++ kori
+  odd check 
+if(i%2==1)
+  count++ kori
+    return kori count
