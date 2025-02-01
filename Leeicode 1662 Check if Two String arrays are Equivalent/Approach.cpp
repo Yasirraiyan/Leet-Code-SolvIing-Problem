97ec5c6f8a7a2087a153951317adbb371ba56095
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-01 21:19:16 +0600

    Create Approach.cpp

---
+++ Leeicode 1662 Check if Two String arrays are Equivalent/Approach.cpp
@@ -0,0 +1,9 @@
+Amar 2 ta string er array  ase.Ei array r string gula k concanate kore 1 ta string e nie compare korbo same kina
+So,1st array traverse r jonno 1 ta for loop chalai i=0 to n porjonto
+2 ta variable nei result 1 result2 string type er ja initial null mane ("")
+protibar array er index value add kori result1 er sathe.
+  Aro 1 ta for loop chalai j=0 to n porjont 2 nd array traverse er jonno
+   //Nested na
+result2 er sathe array 2 er index value jog kori
+Er por 2 ta compare kori == dia same kina same hoile true return kori
+naile false return kori.
