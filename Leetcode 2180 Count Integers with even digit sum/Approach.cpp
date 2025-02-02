8b58ef76712e8ae1b99e0efb55c813def68ba015
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-02 12:42:13 +0600

    Create Approach.cpp

---
+++ Leetcode 2180 Count Integers with even digit sum/Approach.cpp
@@ -0,0 +1,13 @@
+Amar 1 ta integer dea ase.Amar lagbe kotogula integer ase ja less than or equal to num.aro 1 ta condition jar digit sum hoilo even.
+  So,1 theke n porjonto tai 1 ta for loop chalai i=  1 to i<=n porjonto.
+ int  current=i;
+digit sum ber korbo 
+ekta variable nei sum ja initial 0.
+  digit sum so,1 ta while loop chalai check i>0
+  i=i%10;
+sum+=i;
+i=i/10;
+sum even kina tai check 
+if(sum%2==0) hoile
+  count++ kori
+  return kori count.
