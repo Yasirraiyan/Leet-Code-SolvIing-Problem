0d65e0f0b09dbe1d4447ae582cf5c4b29b924e62
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-03 19:30:10 +0600

    Create Approach.cpp

---
+++ Leetcode 2057 Smallest Index with equal value/Approach.cpp
@@ -0,0 +1,11 @@
+Amar 1 ta arrray ase.Amar chaise small index i. Operation i mod 10 == nums[i].
+  So, first e 1nta for loop chalai pura array traverse kori
+check kori 
+   if((i % 10)==nums[i])
+           true hoy tbe 
+if(smallindex==-1||i<smallindex)
+                {
+                  smallindex=i;
+                }
+return smallindex.
+  chaise small index.
