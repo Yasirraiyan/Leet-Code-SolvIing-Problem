dc252f69b765af543decd57dab4716a13bc75bd2
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-04 18:28:46 +0600

    Create Approach.cpp

---
+++ Leetcode 867 Transpose Marix/Approach.cpp
@@ -0,0 +1,6 @@
+Amar 1 ta matrix ase etake transpose korbo.
+  mane column ke row kobo & row k column korbo.
+  So,
+1st e 1 ta for loop chalai i=0 to n porjonto
+row traverse kori.
+  2nd  e 1 ta for loop chalai nested j=0 to n porjonto c
