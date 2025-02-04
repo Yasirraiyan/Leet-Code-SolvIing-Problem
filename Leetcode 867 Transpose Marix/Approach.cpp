05ef62a702b9393a1aea6e1602a45c329dc77a5b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-04 18:34:54 +0600

    Update Approach.cpp

--- Leetcode 867 Transpose Marix/Approach.cpp
+++ Leetcode 867 Transpose Marix/Approach.cpp
@@ -3,4 +3,7 @@ Amar 1 ta matrix ase etake transpose korbo.
   So,
 1st e 1 ta for loop chalai i=0 to n porjonto
 row traverse kori.
-  2nd  e 1 ta for loop chalai nested j=0 to n porjonto c
+  2nd  e 1 ta for loop chalai nested j=0 to n porjonto column traverse kori
+as row column hoy and column row hoy transpose e so,
+ans[j][i]=matrix[i][j] kori
+return kori ans.
