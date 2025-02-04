1cb50eced9effbdf0f7fcaf0b8eb468d7dd95232
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-04 12:54:21 +0600

    Create Approach.cpp

---
+++ Leetcode 2529 Maximum Count of positive integer and negative integer/Approach.cpp
@@ -0,0 +1,14 @@
+Amar 1 ta array ase.Amar array te kisu positive integer ase kisu negative integer ase.Jei type integer beshi ase oi type integer koyta ase ta return korte bolse.
+  positive integer count er jonno 1 ta variable nei pos ja initial 0.
+  negative integer count er jonno 1 ta variable nei neg ja initial 0
+  So, 1 st e ami 1 ta for loop iterate korbo i=0 to i<n porjinto
+Check kori if(nums[i]>0) mane positive hole pos++ kori.
+  if(nums[i]<0) mane positive hole neg++ kori.
+  positive & negative count complete.
+  chaise beshita.
+  check kori 
+if(pos>neg) mane pos beshi
+result=pos;  ami 1 ta variable nai result ja int eta dia result return kori.  Ete subhida hoy.
+ if(pos<neg) mane pos beshi
+result=neg.;   
+  
