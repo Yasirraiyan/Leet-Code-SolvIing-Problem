0490482a17f6f0052450d0a29905ce7b462cb55e
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-05 08:31:40 +0600

    Create Approach.cpp

---
+++ Leetcode 3184 Count Pirs That form a complete day/Approach.cpp
@@ -0,0 +1,10 @@
+Amar 1 ta array ase.Amar lagbe pair kotogula ase ja complet day form kore
+i!=j;
+so,1st e ami 1 ta for loop chalai i=0 to n porjonto.Pura array traverse kori.Then 1 ta nested chalai j=i+1 to n porjonto as i!=j;
+as complete day 1 day 2 day
+  24 dia divisible check
+ if((hours[i]+hours[j])%24==0)
+  kotogula pair
+count++;
+  initial count=0;
+return count;
