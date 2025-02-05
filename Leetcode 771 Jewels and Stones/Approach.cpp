17be47f8c2866ed57252bb65cfdaa089a84678d1
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-05 18:56:03 +0600

    Create Approach.cpp

---
+++ Leetcode 771 Jewels and Stones/Approach.cpp
@@ -0,0 +1,8 @@
+Amar 2 ta string ase jewels and stone.Stone hoilo amar kase thaka string.Jewels hoilo Jewel er kase thaka string.Amar ber korte hbe koto gula stone ase common ja jewel er kaseo ase amar kaseo ase.
+  So, 1 st e 1 ta variable nei count ja initial 0. Ami count korbo common kotogula.
+  1 ta for loop chalai i=0 to jewels.size() porjonto.//Jewel traverse kori
+  Nested for loop chalai j=0 to stones.size() porjonto.//Stones traverse kori
+  check kori common mane equality by index 
+if(stones[i]==jewels[j])
+  true hoile count++ kori 
+  return kori count.
