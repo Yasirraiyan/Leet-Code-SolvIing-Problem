3f98a6b589c5e7248da658dedc0fea34f0ecfd08
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-06 12:50:25 +0600

    Create Approach.cpp

---
+++ Leetcode 2798 Number of Employees Who Met the Target/Approach.cpp
@@ -0,0 +1,6 @@
+Amar 1 ta array ase jar nam hour.Ei hour er ith index value holo i number empployee hour[i] tme kaj kore.Aro 1 ta variable ase target.Amar chaise koto gula employee ase jara target hour er soman ba beshi kaj kore.
+  So, as chaise kotogula ami 1 ta variable nei count jar value initial 0.
+  1 ta for loop chalai i=0 to n porjonto hour array traversre kori.
+  check kori condition 
+if(arr[i]>=hour) true hoy count++ kori
+  return kori count.
