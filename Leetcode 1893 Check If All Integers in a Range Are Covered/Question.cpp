104d30c3440564968057c5287efacad7be0c73e0
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-14 22:26:38 +0600

    Create Question.cpp

---
+++ Leetcode 1893 Check If All Integers in a Range Are Covered/Question.cpp
@@ -0,0 +1,58 @@
+You are given a 2D integer array ranges and two integers left and right. Each ranges[i] = [starti, endi] represents an inclusive interval between starti and endi.
+
+Return true if each integer in the inclusive range [left, right] is covered by at least one interval in ranges. Return false otherwise.
+
+An integer x is covered by an interval ranges[i] = [starti, endi] if starti <= x <= endi.
+
+ 
+
+Example 1:
+
+Input: ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5
+Output: true
+Explanation: Every integer between 2 and 5 is covered:
+- 2 is covered by the first range.
+- 3 and 4 are covered by the second range.
+- 5 is covered by the third range.
+Example 2:
+
+Input: ranges = [[1,10],[10,20]], left = 21, right = 21
+Output: false
+Explanation: 21 is not covered by any range.
+ 
+
+Constraints:
+
+1 <= ranges.length <= 50
+1 <= starti <= endi <= 50
+1 <= left <= right <= 50
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+52.9K
+Submissions
+105.4K
+Acceptance Rate
+50.2%
+Topics
+Companies
+Hint 1
+Hint 2
+Similar Questions
+Discussion (14)
+
+Choose a type
+
+
+
+Copyright ©️ 2025 LeetCode All rights reserved
+
+625
+
+
+14
+
+
+
