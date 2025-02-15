3719eec3791f884183e5a4fe78e51e4b801a98fa
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-02-15 11:05:25 +0600

    Create Approach.cpp

---
+++ Leetcode 2652 Sum Multiples/Approach.cpp
@@ -0,0 +1,10 @@
+Amar 1 ta integer ase n.Amar [1,n] er moddhe oishob integer er sum chaise jara 3 ba 5 ba 7 dara divisible.
+  As sum chaise initially 1 ta variable nei sum ja 0.
+  1 ta for loop chalai karon chaise 1 to n
+ei for loop e i=1 to i<=n porjonto chalai
+condition dei
+ if((i%3==0)||(i%5==0)||(i%7==0))
+  true hoile 
+sum+=i;
+kori
+return kori sum.
