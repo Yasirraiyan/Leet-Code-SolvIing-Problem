5b455fd7fd75ca9e8f534af63c254657f86bbd62
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-03-25 11:56:31 +0600

    Create Solution.cpp

---
+++ Leetcode 2549 Count Distinct Number on a Board/Solution.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+class Solution 
+{
+public:
+    int distinctIntegers(int n) 
+    {
+      if(n==1)
+      {
+        return n;
+      }
+      else
+     return n-1;
+      }
+    
+};
