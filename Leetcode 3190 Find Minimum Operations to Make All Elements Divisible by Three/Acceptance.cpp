dec8a143af2b6f6bc305dfe50e866fa96f7c6f66
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-03-27 12:26:23 +0600

    Update Acceptance.cpp

--- Leetcode 3190 Find Minimum Operations to Make All Elements Divisible by Three/Acceptance.cpp
+++ Leetcode 3190 Find Minimum Operations to Make All Elements Divisible by Three/Acceptance.cpp
@@ -1,1 +1,1 @@
-Accepted Both Solution.Solve successfully by these both solution without seeing any solution or help or hints from another.
+Accepted All Solution.Solve successfully by these all solutions without seeing any solution or help or hints from another.
