27dd6d36392ff168855ce91e536221d393f0251c
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-04-13 16:11:26 +0600

    Update Approach.cpp

--- Leetcode 1629 Slowest Key/Approach.cpp
+++ Leetcode 1629 Slowest Key/Approach.cpp
@@ -115,3 +115,149 @@ Time Complexity:
 
 Space Complexity:
 reserve ভেক্টর: O(n) স্পেস নেয়।
+
+Helper Function:
+-----------------
+  int findmax(vector<int>&arr)
+  {
+    int max=arr[0];
+    for(int m=1;m<arr.size();m++)
+    {
+      if(arr[m]>max)
+      {
+        max=arr[m];
+      }
+    }
+    return max;
+  }
+Main Function:
+--------------
+     char slowestKey(vector<int>& releaseTimes, string keysPressed) 
+    {
+        vector<int>reserve;
+        int ans;
+        char maxKey='\0';
+        vector<int>result;
+        for(int i=0;i<releaseTimes.size();i++)
+        {
+          if(i==0)
+          {
+            reserve.push_back(releaseTimes[0]);
+          }
+          if(i>0)
+          {
+          reserve.push_back(releaseTimes[i]-releaseTimes[i-1]);
+          }
+        }
+        /*for(int j=0;j<reserve.size();j++)
+        {
+          
+             if(findmax(reserve))
+             {
+                result.push_back(reserve[j]);
+             }
+          
+        }
+        */
+        int maxtime=findmax(reserve);
+       
+          for(int m=0;m<reserve.size();m++)
+          {
+            if(reserve[m]==maxtime)
+            {
+              //ans=m;     
+               maxKey = max(maxKey, keysPressed[m]);       // Take lexicographically larger key if there are ties jemon a,c er moddhe pore ba larger lexicographically c 
+                                                         tai eta c return kore
+              }
+          }
+          
+       /* for(int k=0;k<keysPressed.size();k++)
+        {
+          if(k==ans)
+          {
+             resultt = max(resultt, keysPressed[k]); // Take lexicographically larger key if there are ties jemon a,c er moddhe pore ba larger lexicographically c 
+                                                         tai eta c return kore
+            resultt+=keysPressed[k];
+          }
+        }
+        */
+       char resultt='\0';
+        resultt+=maxKey;
+        return resultt;
+    }
+
+Full Code:
+-------------
+
+#include<vector>
+#include<string>
+#include<cctype>
+#include<iostream>
+using namespace std;
+class Solution 
+{
+public:
+  int findmax(vector<int>&arr)
+  {
+    int max=arr[0];
+    for(int m=1;m<arr.size();m++)
+    {
+      if(arr[m]>max)
+      {
+        max=arr[m];
+      }
+    }
+    return max;
+  }
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) 
+    {
+        vector<int>reserve;
+        int ans;
+        char maxKey='\0';
+        vector<int>result;
+        for(int i=0;i<releaseTimes.size();i++)
+        {
+          if(i==0)
+          {
+            reserve.push_back(releaseTimes[0]);
+          }
+          if(i>0)
+          {
+          reserve.push_back(releaseTimes[i]-releaseTimes[i-1]);
+          }
+        }
+        /*for(int j=0;j<reserve.size();j++)
+        {
+          
+             if(findmax(reserve))
+             {
+                result.push_back(reserve[j]);
+             }
+          
+        }
+        */
+        int maxtime=findmax(reserve);
+       
+          for(int m=0;m<reserve.size();m++)
+          {
+            if(reserve[m]==maxtime)
+            {
+              //ans=m;     
+               maxKey = max(maxKey, keysPressed[m]);       
+              }
+          }
+          
+       /* for(int k=0;k<keysPressed.size();k++)
+        {
+          if(k==ans)
+          {
+             resultt = max(resultt, keysPressed[k]); // Take lexicographically larger key if there are ties
+            resultt+=keysPressed[k];
+          }
+        }
+        */
+       char resultt='\0';
+        resultt+=maxKey;
+        return resultt;
+    }
+};
