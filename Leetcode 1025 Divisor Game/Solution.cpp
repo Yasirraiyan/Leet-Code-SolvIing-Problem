afd46022bfaaed63f44a79512ae3dde2fc3534be
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-04-13 22:28:03 +0600

    Create Solution.cpp

---
+++ Leetcode 1025 Divisor Game/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution 
+{
+public:
+    bool divisorGame(int n) 
+{
+     int sum=0;
+     bool win=false;
+       if(n%2==0)
+      {
+         win=true;
+       }
+      return win;
+}
+};
