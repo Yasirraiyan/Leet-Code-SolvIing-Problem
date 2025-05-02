6383dd47f5e70149bb19b1e9de71803e33175ef5
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-05-02 12:16:02 +0600

    Update Acceptance.cpp

--- Leetcode 1480 Running Sum of 1D Array/Acceptance.cpp
+++ Leetcode 1480 Running Sum of 1D Array/Acceptance.cpp
@@ -1,1 +1,1 @@
-Accepted.Solve successfully without seeing any solution or help or hints from another.
+Both solution are Accepted.Solve successfully both  without seeing any solution or help or hints from another.
