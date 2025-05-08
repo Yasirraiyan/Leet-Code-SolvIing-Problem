b259d1af175c54182b881ec4a75cd992e97b0ba2
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-05-08 17:13:56 +0600

    Update Acceptance.cpp

--- Leetcode 1394 Find Lagest Lucky Integer in an arry/Acceptance.cpp
+++ Leetcode 1394 Find Lagest Lucky Integer in an arry/Acceptance.cpp
@@ -1,1 +1,1 @@
-Accepted.Solve succeessfully without seeing any solution or hinths or help from another.
+Both Solution are Accepted.Both are Solved succeessfully by me without seeing any solution or hinths or help from another.
