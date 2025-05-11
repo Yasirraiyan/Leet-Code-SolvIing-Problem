a32574ce90dbbcb38c224035a04d9ddf0ce08114
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-05-11 15:28:04 +0600

    Update Acceptance.cpp

--- Leetcode 3158 Find The XOR of Nums which appear twice/Acceptance.cpp
+++ Leetcode 3158 Find The XOR of Nums which appear twice/Acceptance.cpp
@@ -1,1 +1,1 @@
-Accepted.So;ve successfully without seeing any solution or help or hints from another.
+Both Accepted.Solve both successfully without seeing any solution or help or hints from another.
