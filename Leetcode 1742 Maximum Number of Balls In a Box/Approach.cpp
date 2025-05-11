1e8045df7ba3831755a7e94757462e4cf122cc2d
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-05-11 13:01:02 +0600

    Update Approach.cpp

--- Leetcode 1742 Maximum Number of Balls In a Box/Approach.cpp
+++ Leetcode 1742 Maximum Number of Balls In a Box/Approach.cpp
@@ -260,3 +260,46 @@ max = 2 → ✅ Output: 2
 সেটা কোন box number সেটা ধরে count বাড়াচ্ছো।
 
 শেষে সব box এর মধ্যে কোনটার count বেশি সেটা বের করছো।
+
+  Full Code:
+---------------
+  class Solution 
+{
+public:
+  int digitsum(int n)
+  {
+    int sum=0;
+    while(n>0)
+    {
+      sum+=n%10;
+      n=n/10;
+    }
+    return sum;
+  }
+    int countBalls(int lowLimit, int highLimit)
+     {
+        int n=highLimit-lowLimit+1;
+        unordered_map<int,int>freq;
+        for(int i=lowLimit;i<=highLimit;i++)
+        {
+       
+            freq[digitsum(i)]++;
+         
+        }
+        vector<int>reserve;
+        for(auto it:freq)
+        {
+            reserve.push_back(it.second);
+        }
+        int max=reserve[0];
+        for(int j=1;j<reserve.size();j++)
+        {
+          if(reserve[j]>max)
+          {
+            max=reserve[j];
+          }
+        }
+        return max;
+
+    }
+};
