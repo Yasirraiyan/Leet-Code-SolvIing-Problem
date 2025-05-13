87385bffe206b7bc04067eec9cb9d2c3a0b63272
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-05-13 12:57:02 +0600

    Create Solution2.cpp

---
+++ Leetcode 3289 The Two Sneaky Numbers of Digitvile/Solution2.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums)
+{
+    unordered_map<int,int>freq;
+        for(int i=0;i<nums.size();i++)
+        {
+          freq[nums[i]]++;
+        }
+        vector<int>ans;
+        for(auto it:freq)
+        {
+          if(it.second>1)
+          {
+            ans.push_back(it.first);
+          }
+        }
+        return ans;
+    }
+};
