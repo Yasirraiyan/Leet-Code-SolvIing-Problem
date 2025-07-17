a36c5964585e5bfaba3e153af13cdb4a5874e535
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-07-17 19:49:45 +0600

    Create Solution.cpp

---
+++ Leetcode 540 Single Element in a Sorted Array/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    int singleNonDuplicate(vector<int>& nums)
+    {
+        unordered_map<int,int>freq;
+        int ans;
+        for(int i=0;i<nums.size();i++)
+            {
+                freq[nums[i]]++;
+            }
+        for(auto it:freq)
+            {
+                if(it.second==1)
+                {
+                    ans=it.first;
+                    break;
+                }
+            }
+        return ans;
+    }
+};
