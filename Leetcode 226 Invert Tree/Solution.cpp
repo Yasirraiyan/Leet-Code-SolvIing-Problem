acb0f78d4331afcabd0a7818d7a5ebd168016647
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-08-05 11:08:01 +0600

    Create Solution.cpp

---
+++ Leetcode 226 Invert Tree/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    void invert(TreeNode* root)
+    {
+        if(root==nullptr) return;
+                    TreeNode* temp=root->left;
+                    root->left=root->right;
+                    root->right=temp;
+        invert(root->left);
+        invert(root->right);
+    }
+    TreeNode* invertTree(TreeNode* root) 
+    {
+        invert(root);
+        return root;
+    }
+};
