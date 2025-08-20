3652505add94f75aca314f8cf86a0714f66b3276
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-08-20 16:21:02 +0600

    Create Approach.cpp

---
+++ Leetcode 3115 Maximum Prime Difference/Approach.cpp
@@ -0,0 +1,9 @@
+Test korbo array r kon index prime tader moddhe stored number gula nibo array ye allada kore.
+  Max diff chaise
+So,
+  max diff=maxval-minval
+  karon
+[1,2,3,4,5]
+  beshi diff=5-1=4;=max-min
+stored areay r max ber kori min ber kori 
+duitar diff ber kore return kori
