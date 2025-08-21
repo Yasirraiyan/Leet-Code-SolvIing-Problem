2f4eac9ec4ae31a83bb211e41c8ba9ac74e165be
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-08-21 15:55:29 +0600

    Create Solution.cpp

---
+++ Leetcode 374 Guess Number Higgher or Lower/Solution.cpp
@@ -0,0 +1,35 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution 
+{
+public:
+    int guessNumber(int n) 
+    {
+        int low=0;
+        int high=n;
+        int mid;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int res=guess(mid);
+            if(-1==res) 
+            {
+            high=mid-1;
+            }
+           else if(1==res)
+           {
+            low=mid+1;
+           }
+            else if(res==0) return mid;
+           
+        }
+        return low;
+    }
+};
